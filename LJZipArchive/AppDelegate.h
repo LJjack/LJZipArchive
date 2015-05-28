1230c80057d33507a987af511369245bdf6d4adc
刘俊杰 <945980335@qq.com>
2015-05-28 16:20:47 +0800

    Initial Commit

---
+++ LJZipArchive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJZipArchive
+//
+//  Created by liujunjie on 15-5-28.
+//  Copyright (c) 2015年 rj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
